e8b38c6133c933f95ab869289526f7c90960dc49
yossi katzman <issoyk@gmail.com>
2018-03-04 19:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeliLine= [];
 
 function takeANumber(katzDeli, name) {
   for (var i = 0, a = katzDeli.length ;i < a  ; a++) {
-    katzDeliLine.push(katzDeliLine[a])
+    katzDeliLine.push(katzDeliLine[i])
   }
   katzDeliLine.push(name)
   var n = katzDeliLine.length
