face1b0146c984a081e2e3162febe051286f6ec7
yossi katzman <issoyk@gmail.com>
2018-03-04 20:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var katzDeliLine = [];
-
-
 
 function takeANumber(katzDeli, name) {
-  katzDeliLine.push(katzDeli, name)
-  var n = katzDeliLine.length
-    return `Welcome, ${name}. You are number ${n} in line.`;
+  katzDeli.push(name)
+  return `Welcome, ${name}. You are number ${n} in line.`;
 
 }
