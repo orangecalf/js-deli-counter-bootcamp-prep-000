0ce7e772e948790de573d2bc4eae4971812df744
yossi katzman <issoyk@gmail.com>
2018-03-04 20:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var katzDeliLine= [];
+var katzDeliLine = [];
 
 
 function takeANumber(katzDeli, name) {
-  for (var i = 0, a = katzDeli.length ;i < a  ; i++) {
-    katzDeliLine.push(katzDeliLine[i])
-  }
+  katzDeliLine.push(katzDeliLine)
   katzDeliLine.push(name)
   var n = katzDeliLine.length
   return `Welcome, ${name}. You are number ${n} in line.`;
